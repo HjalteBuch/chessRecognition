f88490014a9a65f4750d2e2eab48a64273741f3c
HjalteBuch <h.jaltehb123@gmail.com>
2021-01-14 12:30:53 +0100

    added OpenCV library

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
+
 public class Main extends Application {
+
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,6 +22,9 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
+
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
 
     public static void main(String[] args) {
